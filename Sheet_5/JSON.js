8bf74ed8a82d3f4ca6280a71de98a2aeb069e3b8
prit-patel2003 <pritp@solguruz.com>
2024-02-28 15:27:43 +0530

    sheet 5 completed

--- Sheet_5/JSON.js
+++ Sheet_5/JSON.js
@@ -32,6 +32,8 @@ function Read(){
 function Delete(){
     document.getElementById("form").style.display="none"
     document.getElementById("deletable").style.display="inline-block"
+    document.getElementById("Companytable").style.display="none"
+
   
 }
 function deletename(){
@@ -51,6 +53,7 @@ function deleteid(){
 }
 function deleteparticular(){
     let d=document.getElementById("inputparticular").value
+
     let index=JSON1.findIndex((item)=>item.name===d || item.id===d)
     if (confirm("Are you sure you want to delete the data") == true) {
         JSON1.splice(index, 1);
@@ -64,11 +67,15 @@ function deleteparticular(){
     
     
 }
-function val(id,partno,name,size,checkedValues,desc){
+function val(id,partno,name,size,checkedValues,desc,weight,companyname,address,date){
+    
+
+
     let add=false;
     let add1=false;
     let add2=false;
     let add3=false;
+    
     
     let existproductid = JSON1.some((item, index) => index !== editIndex && item.id === id);
     let existdata= JSON1.some((item, index) => index !== editIndex && item.name.toLowerCase() === name.toLowerCase());
@@ -122,6 +129,35 @@ function val(id,partno,name,size,checkedValues,desc){
     } else {
         document.getElementById("partval").innerHTML = "";
         document.getElementById("partNo").style.border = "none";
+        add1 = true;
+    }
+    let pattern2=/^(?!0\d)\d+(\.\d+)?$/;
+
+    if (!pattern2.test(String(weight))) {
+        if (weight < 0) {
+            document.getElementById("partweightval").innerHTML = "Product weight cannot be negative";
+            document.getElementById("partweight").style.border = "2px solid red";
+            add1 = false;
+        } 
+        if(!pattern2.test(weight) && flag===false){ 
+            if(String(weight).length===0){
+                document.getElementById("partweightval").innerHTML = "Weight cannot be zero";
+                document.getElementById("partweight").style.border = "2px solid red";
+                add1 = false;
+            }else{
+
+                document.getElementById("partweightval").innerHTML = "NO special character allowed or preceding zeroes or zero allowed";
+                document.getElementById("partweight").style.border = "2px solid red";
+                add1 = false;
+            }
+        }
+    } else if (weight >= 1000) {
+        document.getElementById("partweightval").innerHTML = "Product weight should be less than 999 kg";
+        document.getElementById("partweight").style.border = "2px solid red";
+        add1 = false;
+    } else {
+        document.getElementById("partweightval").innerHTML = "";
+        document.getElementById("partweight").style.border = "none";
         add1 = true;
     }
     if (name.length !== 0) {
@@ -180,6 +216,71 @@ function val(id,partno,name,size,checkedValues,desc){
         document.getElementById("desc").style.border = "none";
         add3 = true;
     }
+
+    if (address.length===0 && flagcompany===false) {
+        document.getElementById("addressval").innerHTML = "Address is empty";
+        document.getElementById("address").style.border = "2px solid red";
+        add3 = false;
+    } else if (address.length >= 300) {
+        document.getElementById("addressval").innerHTML = "Address should be less than 300";
+        document.getElementById("address").style.border = "2px solid red";
+        add3 = false;
+    } else {
+        document.getElementById("addressval").innerHTML = "";
+        document.getElementById("address").style.border = "none";
+        add3 = true;
+    }
+
+    if (companyname.length !== 0) {
+        const pattern = /^(?![\s])[a-zA-Z]+(?:[\s]{1}[a-zA-Z]+)*[\s]*$/;
+        if (companyname.length > 0) {
+            if (companyname[0] === " ") {
+                document.getElementById("companynameval").innerHTML = "No spaces at start";
+                document.getElementById("companyname").style.border = "2px solid red";
+                add2 = false;
+            } else if (!pattern.test(companyname)) {
+                document.getElementById("companynameval").innerHTML = "Company name should contain only letters and single spaces";
+                document.getElementById("companyname").style.border = "2px solid red";
+                add2 = false;
+            }
+            else if (companyname.length > 20) {
+                document.getElementById("companynameval").innerHTML = "Company name should be less than 50 character";
+                document.getElementById("companyname").style.border = "2px solid red";
+                add2 = false;
+            } else {
+                document.getElementById("companynameval").innerHTML = "";
+                document.getElementById("companyname").style.border = "none";
+                add2 = true;
+            }
+        }
+    } else if (companyname.length == 0 && flagcompany===false) {
+        document.getElementById("companynameval").innerHTML = "Company name  is empty";
+        document.getElementById("companyname").style.border = "2px solid red";
+        add2 = false;
+    } else {
+        document.getElementById("companynameval").innerHTML = "";
+        document.getElementById("companyname").style.border = "none";
+        add2 = true;
+    }
+    let selectedDate = new Date(date);
+    let currentDate = new Date();
+    if(!date && flagcompany===false){
+        document.getElementById("dateval").innerHTML = "date is empty";
+            document.getElementById("date").style.border = "2px solid red";
+            add2=false
+
+        }else if(selectedDate > currentDate){
+            document.getElementById("dateval").innerHTML = "Establish date is in future";
+            document.getElementById("date").style.border = "2px solid red";
+            add2=false
+        }
+        else{
+            document.getElementById("dateval").innerHTML = "";
+            document.getElementById("date").style.border = "";
+            add2=true
+        }
+
+    
     if(add && add1 && add2 && add3){
         return true
     }
@@ -221,6 +322,8 @@ function edit() {
     document.getElementById("edittable").style.display="inline-block"
     document.getElementById("form").style.display="none"
     document.getElementById("editbtn3").innerText = "Update Weight";
+    document.getElementById("Companytable").style.display="none"
+
 
 }
 function editname(){
@@ -306,8 +409,11 @@ function UpdateProduct(index, id, partno, name, size, color,desc,weight,companyn
     JSON1[index].weight = weight;
     JSON1[index].details[0] = companyname;
     JSON1[index].details[1] = address;
-    JSON1[index].details[2] = new Date(date)    ;
-
+    JSON1[index].details[2] = new Date(date);
+    let checked = document.querySelectorAll('input[type="checkbox"]:checked');
+    let checkedValues = [...checked].map(c => c.value).join(' ');
+    JSON1[index].size=document.querySelector('input[type="radio"]:checked').value;
+    JSON1[index].color=checkedValues
     
 
     document.getElementById("reset").style.display = "inline-block";
@@ -388,7 +494,7 @@ function printData(JSON1){
             tr += "<td>" + JSON1[i].weight + "</td>";
 
         }
-        if(JSON1[i].details[0]==="NA"){
+        if(JSON1[i].details[0]==="NA" || JSON1[i].details[0]===""){
             tr += "<td>" + "-" + "</td>";
         }
         else{
@@ -418,52 +524,44 @@ function search(){
         printData(JSON1);
     }
 }
-function sort(){
-    let sort=document.getElementById("sortvalue").value;
-    switch (sort){
-        case  "partIdSortup":
-            JSON1.sort((a,b)=>b.id-a.id)
+function sort() {
+    let sort = document.getElementById("sortvalue").value;
+    switch (sort) {
+        case "partIdSortup":
+            JSON1.sort((a, b) => b.id - a.id);
             break;
         case "partIdSortdown":
-            JSON1.sort((a,b)=>a.id-b.id)
+            JSON1.sort((a, b) => a.id - b.id);
             break;
         case "partNoSortup":
-            JSON1.sort((a,b)=>b.partno-a.partno)
+            JSON1.sort((a, b) => b.partno - a.partno);
             break;
         case "partNoSortdown":
-            JSON1.sort((a,b)=>a.partno-b.partno)
+            JSON1.sort((a, b) => a.partno - b.partno);
             break;
         case "partNameSort":
-            JSON1.sort((a,b)=>{
-                if (a.name < b.name) {
-                    return -1;
-                  }
-                  if (a.name > b.name) {
-                    return 1;
-                  }
-                  return 0;
-            })
+            JSON1.sort((a, b) => a.name.localeCompare(b.name));
+            break;
         case "Date":
-            JSON1.sort((a,b)=>a.details[2]-b.details[2])
+            JSON1.sort((a, b) => new Date(a.details[2]) - new Date(b.details[2]));
             break;
         case "Namelength":
-            JSON1.sort((a,b)=>{
-                if (a.details[0].length < b.details[0].length) {
+            JSON1.sort((a, b) => {
+                if (a.details[0].length < b.details[0].length || a.details[0] === "NA") {
                     return -1;
-                  }
-                  if (a.details[0].length > b.details[0].length) {
+                }
+                if (a.details[0].length > b.details[0].length || b.details[0] === "NA") {
                     return 1;
-                  }
-                  return 0;
-            })
+                }
+                return 0;
+            });
             break;
-        
-
     }
-    printData(JSON1)
-    
-    
+    // Move rows with details[0] as "NA" to the end
+    // JSON1.sort((a, b) => (a.details[0] === "NA") - (b.details[0] === "NA"));
+    printData(JSON1);
 }
+
 function addweight(){
     
     if(flag){
@@ -476,7 +574,8 @@ function addweight(){
         flag=true;
         document.getElementById("editbtn3").innerText="Add Weight"
         document.getElementById("weightinput").style.display="none"
-
+        document.getElementById("partweightval").innerHTML = "";
+        document.getElementById("partweight").style.border = "";
 
     }
 
@@ -497,7 +596,8 @@ function addcompany(){
         flagcompany=true
         document.getElementById("Companytable").style.display="none"
         document.getElementById("editbtn4").innerText="Add details"
-
+        document.getElementById("addressval").innerHTML = "";
+        document.getElementById("address").style.border = "none";
 
     }
     
